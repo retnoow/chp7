7043c6d9f3d37026324198622bb90535c78e5034
Retno Wati <retnowati383@gmail.com>
2022-07-23 23:14:32 +0700

    adding relation model

--- models/user_game_biodata.js
+++ models/user_game_biodata.js
@@ -3,26 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user_game extends Model {
+  class user_game_biodata extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasOne(models.user_game_biodata, {foreignKey: "id_user"})
+      // define association here
     }
   }
-  user_game.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isSuperAdmin: {
-      defaultValue: false,
-      type: DataTypes.BOOLEAN
-    }
+  user_game_biodata.init({
+    id_user: DataTypes.INTEGER,
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    birthplace: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'user_game',
+    modelName: 'user_game_biodata',
   });
-  return user_game;
+  return user_game_biodata;
 };
--- models/user_game_history.js
+++ models/user_game_history.js
@@ -3,26 +3,23 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user_game extends Model {
+  class user_game_history extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasOne(models.user_game_biodata, {foreignKey: "id_user"})
+      // define association here
     }
   }
-  user_game.init({
-    username: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isSuperAdmin: {
-      defaultValue: false,
-      type: DataTypes.BOOLEAN
-    }
+  user_game_history.init({
+    id_user: DataTypes.INTEGER,
+    point: DataTypes.INTEGER,
+    played_at: DataTypes.DATE
   }, {
     sequelize,
-    modelName: 'user_game',
+    modelName: 'user_game_history',
   });
-  return user_game;
+  return user_game_history;
 };
